c0af397ffc405b2d453a5ce9fb2a46559053d3ae
Jonathan <jmkeiste@live.com>
2018-07-01 13:00:22 -0400

    Added models

---
+++ src/HookYarnSequinsGallery/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HookYarnSequinsGallery.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Artist[] Artists { get; set; }
+        public bool Favorite { get; set; }
+    }
+}
